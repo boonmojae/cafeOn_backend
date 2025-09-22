8e380eb7e18ba7f5a83d5e7d976aaf8bc0925ed5
boonmojae <pminny99@naver.com>
2025-09-22 15:57:31 +0900

    feat: Initial project setup

---
+++ src/main/java/com/b1a4/cafeOn/CafeOnApplication.java
@@ -0,0 +1,13 @@
+package com.b1a4.cafeOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOnApplication.class, args);
+	}
+
+}
