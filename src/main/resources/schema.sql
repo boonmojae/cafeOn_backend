1cf2d2a8afb4a83bb4b16c15f99d798b1f1bafdf
Github d0ikim <doyclub@naver.com>
2025-10-26 14:56:27 +0900

    fix: cafes 테이블에서 미사용중인 naver_rating 컬럼 삭제 & reviews_summary 로 컬럼이름 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -34,11 +34,10 @@ CREATE TABLE IF NOT EXISTS cafes (
     phone VARCHAR(50), -- 전화번호
     open_hours TEXT, -- 오픈 시간
     kakao_rating DECIMAL(3,2), -- 카카오맵 내 후기별점
-    naver_rating DECIMAL(3,2), -- 네이버지도 내 후기별점
     avg_rating DECIMAL(3,2), -- 카카오맵과 네이버지도의 별점을 평균낸 최종별점(ex. 3.44)
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     kakao_url VARCHAR(255), -- 카카오맵 URL
-    reviewsSummary TEXT,    -- 네이버블로그에 카페이름 검색해 나온 내용들을 ai로 한줄요약함
+    reviews_summary TEXT,    -- 네이버블로그에 카페이름 검색해 나온 내용들을 ai로 한줄요약함
     source ENUM('KAKAO', 'USER') DEFAULT 'KAKAO' -- 데이터 출처
 );
 
