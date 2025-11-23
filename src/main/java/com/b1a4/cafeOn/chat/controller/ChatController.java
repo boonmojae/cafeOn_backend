da2e8c8a44f4ee4994b70d50f48939b4c71f4eee
boonmojae <pminny99@naver.com>
2025-11-23 19:01:16 +0900

    fix: chat

--- src/main/java/com/b1a4/cafeOn/chat/controller/ChatController.java
+++ src/main/java/com/b1a4/cafeOn/chat/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.b1a4.cafeOn.chat.controller;
 
 import com.b1a4.cafeOn.chat.dto.chat.ChatRequestDTO;
-import com.b1a4.cafeOn.chat.dto.chat.ChatResponseDTO;
 import com.b1a4.cafeOn.chat.service.ChatService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,13 +29,8 @@ public class ChatController {
 
         log.info("[WS][SEND-IN ] roomId={}, senderId={}, msg={}", roomId, senderId, chatRequestDTO.message());
 
-        ChatResponseDTO save = chatService.saveChat(roomId, senderId, chatRequestDTO);
+        chatService.saveChat(roomId, senderId, chatRequestDTO);
 
-        log.info("[WS][SEND-OUT] roomId={}, dto.senderId={}, dto.type={}, dto.mine={}",
-                roomId, save.getSenderId(), save.getMessageType(), save.getMine());
-
-        // 구독자한테 브로드캐스트
-        template.convertAndSend("/sub/rooms/" + roomId, save);
     }
 
 }
--- src/main/java/com/b1a4/cafeOn/config/websocket/WebSocketBrokerConfig.java
+++ src/main/java/com/b1a4/cafeOn/config/websocket/WebSocketBrokerConfig.java
@@ -14,12 +14,6 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 @Configuration
 public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final ThreadPoolTaskScheduler brokerTaskScheduler;
-
-    public WebSocketBrokerConfig(@Qualifier("customBrokerTaskScheduler")ThreadPoolTaskScheduler brokerTaskScheduler) {
-        this.brokerTaskScheduler = brokerTaskScheduler;
-    }
-
     // 기본 messageBrokerTaskScheduler 과 겹치지 않도록 커스텀 이름 사용
     @Bean(name = "customBrokerTaskScheduler")
     public ThreadPoolTaskScheduler customBrokerTaskScheduler() {
@@ -51,7 +45,7 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
         registry.setUserDestinationPrefix("/user");
 
         registry.enableSimpleBroker("/sub", "/queue") // 방 브로드캐스트, 개인 큐
-                .setTaskScheduler(brokerTaskScheduler)
+                .setTaskScheduler(customBrokerTaskScheduler())
                 // .setHeartbeatValue(new long[]{10_000, 10_000}); 엄격한 설정
                 .setHeartbeatValue(new long[]{10_000, 0}); // 서버 -> 클라만 보내고, 크라 -> 서버 기대는 해제
     }
